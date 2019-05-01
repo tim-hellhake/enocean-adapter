cd8e5574b2e3a0d86055ec55e743987b58a33a45
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2019-05-01 23:46:54 +0200

    Release initial version 0.1.0

---
+++ README.md
@@ -0,0 +1,24 @@
+# Enocean Adapter
+
+[![Build Status](https://travis-ci.org/tim-hellhake/enocean-adapter.svg?branch=master)](https://travis-ci.org/tim-hellhake/enocean-adapter)
+[![dependencies](https://david-dm.org/tim-hellhake/enocean-adapter.svg)](https://david-dm.org/tim-hellhake/enocean-adapter)
+[![devDependencies](https://david-dm.org/tim-hellhake/enocean-adapter/dev-status.svg)](https://david-dm.org/tim-hellhake/enocean-adapter?type=dev)
+[![optionalDependencies](https://david-dm.org/tim-hellhake/enocean-adapter/optional-status.svg)](https://david-dm.org/tim-hellhake/enocean-adapter?type=optional)
+[![license](https://img.shields.io/badge/license-MPL--2.0-blue.svg)](LICENSE)
+
+Connect your enocean devices.
+
+## Supported devices
+### Gateways
+- USB 300 stick
+### Devices
+- F6-04-xx Switches (e.g. Eltako FT55)
+
+## Configuration
+All you need to configure is the path to the serial port of the usb stick.
+
+## Teach in device
+- Go to the _Add things_
+- Press a button on the switch
+- A new thing should appear
+- Add it
---
+++ enocean-adapter.js
@@ -0,0 +1,110 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+ */
+
+'use strict';
+
+const enocean = require('node-enocean')();
+
+const {
+  Adapter,
+  Device,
+  Property
+} = require('gateway-addon');
+
+class PushButton extends Device {
+  constructor(adapter, senderId) {
+    super(adapter, `${PushButton.name}-${senderId}`);
+    this['@context'] = 'https://iot.mozilla.org/schemas/';
+    this['@type'] = 'PushButton';
+    this.name = this.id;
+    this.description = 'Enocean push button';
+
+    this.keyCodes = {
+      10: 'A1',
+      30: 'A0',
+      50: 'B1',
+      70: 'B0',
+    };
+
+    for (const keyCode in this.keyCodes) {
+      const name = this.keyCodes[keyCode];
+      const description = {
+        '@type': 'PushedProperty',
+        title: name,
+        description: 'If the button is pressed',
+        type: 'boolean',
+        readOnly: true
+      };
+
+      this.properties.set(name, new Property(this, name, description));
+    }
+  }
+
+  handle(data) {
+    if (!data.raw) {
+      console.warn('Data contains no value');
+      return;
+    }
+
+    if (data.raw === '00') {
+      console.log('Button up');
+      for (const pair of this.properties) {
+        const property = pair[1];
+        property.setCachedValue(false);
+        this.notifyPropertyChanged(property);
+      }
+      return;
+    }
+
+    const name = this.keyCodes[data.raw];
+
+    if (name) {
+      console.log(`Button ${name} down`);
+      const property = this.properties.get(name);
+      property.setCachedValue(true);
+      this.notifyPropertyChanged(property);
+      return;
+    }
+
+    console.warn(`Unknown key code ${data.raw}`);
+  }
+}
+
+class EnoceanAdapter extends Adapter {
+  constructor(addonManager, manifest) {
+    super(addonManager, EnoceanAdapter.name, manifest.name);
+    addonManager.addAdapter(this);
+    const knownDevices = {};
+    const serialPort = manifest.moziot.config.serialPort;
+
+    if (!serialPort) {
+      console.warn('Please specify serial port in the config');
+      return;
+    }
+
+    enocean.listen(serialPort);
+
+    enocean.on('data', (data) => {
+      console.log(`Received ${JSON.stringify(data)}`);
+      if (data.choice === 'f6') {
+        const knownDevice = knownDevices[data.senderId];
+
+        if (knownDevice) {
+          knownDevice.handle(data);
+        } else {
+          console.log(`Detected new push button ${data.senderId}`);
+          const device = new PushButton(this, data.senderId);
+          knownDevices[data.senderId] = device;
+          this.handleDeviceAdded(device);
+        }
+      } else {
+        console.log(`Unknown telegram type ${data.choice}`);
+      }
+    });
+  }
+}
+
+module.exports = EnoceanAdapter;
