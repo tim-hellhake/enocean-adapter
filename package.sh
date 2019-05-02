0faf98e5502244b3f6d919bafec8605deb7d1126
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2019-05-02 18:16:29 +0200

    Add crosscompiler support

--- package.sh
+++ package.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-npm install --production
+rm -rf node_modules
+
+if [ -z "${ADDON_ARCH}" ]; then
+  TARFILE_SUFFIX=
+else
+  NODE_VERSION="$(node --version)"
+  TARFILE_SUFFIX="-${ADDON_ARCH}-${NODE_VERSION/\.*/}"
+fi
+
+if [ "${ADDON_ARCH}" == "linux-arm" ]; then
+  # We assume that CC and CXX are pointing to the cross compilers
+  npm install --ignore-scripts --production
+  npm rebuild --arch=armv6l --target_arch=arm
+else
+  npm install --production
+fi
 
 shasum --algorithm 256 package.json *.js LICENSE > SHA256SUMS
 find node_modules -type f -exec shasum --algorithm 256 {} \; >> SHA256SUMS
